2c5442a972d93dce29ea918782c0912b21fa719f
AES <amirhoseinjalian82@gmail.com>
2023-06-07 00:50:13 +0330

    main app created

---
+++ src/main/java/com/jalian/tdd/MainApp.java
@@ -0,0 +1,9 @@
+package com.jalian.tdd;
+
+public class MainApp {
+    public static void main(String[] args) {
+        for(int i = 1; i < 100; i++) {
+            System.out.println(i + ", " + FizzBuzz.compute(i));
+        }
+    }
+}
