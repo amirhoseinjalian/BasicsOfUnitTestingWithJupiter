7eb760dcdfb56f87b2db49156574708d65cccaab
AES <amirhoseinjalian82@gmail.com>
2023-06-07 00:49:18 +0330

    creating fizz buzz test

--- src/test/java/com/jalian/tdd/FizzBuzzTest.java
+++ src/test/java/com/jalian/tdd/FizzBuzzTest.java
@@ -2,7 +2,10 @@ package com.jalian.tdd;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 class FizzBuzzTest {
@@ -10,6 +13,49 @@ class FizzBuzzTest {
     @Test
     @DisplayName("Divisible By Three")
     void testForDivisibleByThree() {
-        fail("fail");
+        String expected = "Fizz";
+        assertEquals(expected, FizzBuzz.compute(3), "should be divisible by 3");
+    }
+
+    @Test
+    @DisplayName("Divisible By Fife")
+    void testForDivisibleByFive() {
+        String expected = "Buzz";
+        assertEquals(expected, FizzBuzz.compute(10), "should be divisible by 5");
+    }
+
+    @Test
+    @DisplayName("Divisible By Three And Five")
+    void testForDivisibleByThreeAndFive() {
+        String expected = "FizzBuzz";
+        assertEquals(expected, FizzBuzz.compute(15), "should be divisible by 3 and 5");
+    }
+
+    @Test
+    @DisplayName("Not Divisible By Three or Five")
+    void testForNotDivisibleByThreeOrFive() {
+        String expected = "1";
+        assertEquals(expected, FizzBuzz.compute(1), "should be not divisible by 3 or 5");
+    }
+
+    @ParameterizedTest(name = "value={0}, expected={1}")
+    @CsvFileSource(resources = "/small-test-data.csv")
+    @DisplayName("Testing With Small Data File")
+    void testSmallDataFile(int value, String expected) {
+        assertEquals(expected, FizzBuzz.compute(value), "should be not divisible by 3 or 5");
+    }
+
+    @ParameterizedTest(name = "value={0}, expected={1}")
+    @CsvFileSource(resources = "/medium-test-data.csv")
+    @DisplayName("Testing With Medium Data File")
+    void testMediumDataFile(int value, String expected) {
+        assertEquals(expected, FizzBuzz.compute(value), "should be not divisible by 3 or 5");
+    }
+
+    @ParameterizedTest(name = "value={0}, expected={1}")
+    @CsvFileSource(resources = "/large-test-data.csv")
+    @DisplayName("Testing With large Data File")
+    void testLargeDataFile(int value, String expected) {
+        assertEquals(expected, FizzBuzz.compute(value), "should be not divisible by 3 or 5");
     }
 }
