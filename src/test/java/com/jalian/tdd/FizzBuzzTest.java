9fe8d348919b539b2bf39d826c7cf481cd1f72d7
AES <amirhoseinjalian82@gmail.com>
2023-06-06 21:39:34 +0330

    creating fizz buzz class

---
+++ src/test/java/com/jalian/tdd/FizzBuzzTest.java
@@ -0,0 +1,15 @@
+package com.jalian.tdd;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class FizzBuzzTest {
+
+    @Test
+    @DisplayName("Divisible By Three")
+    void testForDivisibleByThree() {
+        fail("fail");
+    }
+}
